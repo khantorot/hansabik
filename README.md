6e315534b88f70f25ed4f8ccb3107ec7ea05cfa6
khantorot <khantorot@gmail.com>
2023-01-10 05:17:35 +0600

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+<br />
+<div align="center">
+  <a href="#top">
+    <img src="content/icons/logo.png" alt="logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">hansabik</h3>
+
+  <p align="center">
+    Fictional Charitable Foundation.
+    <br />
+    <br />
+    <a href="https://khantorot.github.io/hansabik">view project +</a>
+  </p>
+</div>
+<br />
+
+
+
+
+
+### about
+
+Temporary content was taken from an unknown resource.
+
+
+
+<br />
+
+
+
+### contact
+
+Email - khantorot@gmail.com
+
+
+
+
+
+<br />
+<p align="center"><a href="#top">back to top ↑</a></p>
---
+++ assets/js/script.js
@@ -0,0 +1,178 @@
+const current_location = location.href;
+const menu_item = document.querySelectorAll('.menu a');
+const menu_length = menu_item.length;
+
+for (let i = 0; i < menu_length; i++) {
+    if (menu_item[i].href === current_location) {
+        menu_item[i].className = 'active';
+    }
+}
+
+
+const menu = document.querySelector('nav .menu');
+const meun_btn = document.querySelector('nav .menu_btn');
+
+meun_btn.addEventListener('click', function () {
+    menu.classList.toggle('menu_show');
+})
+
+
+
+
+
+
+
+
+
+
+document.querySelectorAll('.button').forEach(button => {
+
+    let getVar = variable => getComputedStyle(button).getPropertyValue(variable);
+
+    button.addEventListener('click', e => {
+
+        e.preventDefault();
+
+        if (!button.classList.contains('active')) {
+
+            button.classList.add('active');
+
+            gsap.to(button, {
+                keyframes: [{
+                    '--left-wing-first-x': 50,
+                    '--left-wing-first-y': 100,
+                    '--right-wing-second-x': 50,
+                    '--right-wing-second-y': 100,
+                    duration: .2,
+                    onComplete() {
+                        gsap.set(button, {
+                            '--left-wing-first-y': 0,
+                            '--left-wing-second-x': 40,
+                            '--left-wing-second-y': 100,
+                            '--left-wing-third-x': 0,
+                            '--left-wing-third-y': 100,
+                            '--left-body-third-x': 40,
+                            '--right-wing-first-x': 50,
+                            '--right-wing-first-y': 0,
+                            '--right-wing-second-x': 60,
+                            '--right-wing-second-y': 100,
+                            '--right-wing-third-x': 100,
+                            '--right-wing-third-y': 100,
+                            '--right-body-third-x': 60
+                        })
+                    }
+                }, {
+                    '--left-wing-third-x': 20,
+                    '--left-wing-third-y': 90,
+                    '--left-wing-second-y': 90,
+                    '--left-body-third-y': 90,
+                    '--right-wing-third-x': 80,
+                    '--right-wing-third-y': 90,
+                    '--right-body-third-y': 90,
+                    '--right-wing-second-y': 90,
+                    duration: .2
+                }, {
+                    '--rotate': 50,
+                    '--left-wing-third-y': 95,
+                    '--left-wing-third-x': 27,
+                    '--right-body-third-x': 45,
+                    '--right-wing-second-x': 45,
+                    '--right-wing-third-x': 60,
+                    '--right-wing-third-y': 83,
+                    duration: .25
+                }, {
+                    '--rotate': 60,
+                    '--plane-x': -8,
+                    '--plane-y': 40,
+                    duration: .2
+                }, {
+                    '--rotate': 40,
+                    '--plane-x': 45,
+                    '--plane-y': -300,
+                    '--plane-opacity': 0,
+                    duration: .375,
+                    onComplete() {
+                        setTimeout(() => {
+                            button.removeAttribute('style');
+                            gsap.fromTo(button, {
+                                opacity: 0,
+                                y: -8
+                            }, {
+                                opacity: 1,
+                                y: 0,
+                                clearProps: true,
+                                duration: .3,
+                                onComplete() {
+                                    button.classList.remove('active');
+                                }
+                            })
+                        }, 1800)
+                    }
+                }]
+            })
+
+            gsap.to(button, {
+                keyframes: [{
+                    '--text-opacity': 0,
+                    '--border-radius': 0,
+                    '--left-wing-background': getVar('--primary-dark'),
+                    '--right-wing-background': getVar('--primary-dark'),
+                    duration: .11
+                }, {
+                    '--left-wing-background': getVar('--primary'),
+                    '--right-wing-background': getVar('--primary'),
+                    duration: .14
+                }, {
+                    '--left-body-background': getVar('--primary-dark'),
+                    '--right-body-background': getVar('--primary-darkest'),
+                    duration: .25,
+                    delay: .1
+                }, {
+                    '--trails-stroke': 171,
+                    duration: .22,
+                    delay: .22
+                }, {
+                    '--success-opacity': 1,
+                    '--success-x': 0,
+                    duration: .2,
+                    delay: .15
+                }, {
+                    '--success-stroke': 0,
+                    duration: .15
+                }]
+            })
+
+        }
+
+    })
+
+});
+
+
+
+
+
+let linkNav = document.querySelectorAll('[href^="#"]'),
+    V = 0.222;
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        let w = window.pageYOffset,
+            hash = this.href.replace(/[^#]*(.*)/, '$1');
+        t = document.querySelector(hash).getBoundingClientRect().top,
+            start = null;
+        requestAnimationFrame(step);
+
+        function step(time) {
+            if (start === null) start = time;
+            let progress = time - start,
+                r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+            window.scrollTo(0, r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash
+            }
+        }
+    }, false);
+}
